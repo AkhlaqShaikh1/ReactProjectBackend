40d39ab5f71e4a564a0d87402fae728c85a85a70
Akhlaq Ahmed Shaikh <akhlaqshaikh03@gmail.com>
2024-05-01 23:39:18 +0500

    Ready I hope so

--- api/index.js
+++ api/index.js
@@ -1,13 +1,87 @@
 const express = require("express");
 const connect = require("../Database/config");
-const routes = require("../index");
+const jobModel = require("../Database/model/jobModel");
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 connect();
 
-app.use("/", routes);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/jobs", async (req, res) => {
+  try {
+    const jobs = await jobModel.find();
+    res.json(jobs);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+app.put("/jobs/:id", async (req, res) => {
+  const job = await Job.findById(req.params.id);
+  if (!job) return res.status(404).send("Job not found");
+  try {
+    const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedJob);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+app.delete("/jobs/:id", async (req, res) => {
+  try {
+    const job = await Job.findById(req.params.id);
+    if (!job) {
+      console.log("Job not found");
+      return res.status(404).json({ message: "Job not found" });
+    }
+    await job.deleteOne();
+    res.status(200).json({ message: `Job deleted with ${req.params.id}` });
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+app.get("/jobs/:id", async (req, res) => {
+  try {
+    const job = await Job.findById(req.params.id);
+    if (!job) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+    res.json(job);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+app.post("/add-job", async (req, res) => {
+  const job = new Job({
+    title: req.body.title,
+    type: req.body.type,
+    description: req.body.description,
+    location: req.body.location,
+    salary: req.body.salary,
+    company: {
+      name: req.body.company.name,
+      description: req.body.company.description,
+      contactEmail: req.body.company.contactEmail,
+      contactPhone: req.body.company.contactPhone,
+    },
+  });
+
+  try {
+    const savedJob = await job.save();
+    res.json(savedJob);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
